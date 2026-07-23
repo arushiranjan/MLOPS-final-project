142886333c342cf64a00dfb1ff7d96e96a9367cb
Arushi Ranjan <arushi.ranjan26@gmail.com>
2026-07-23 10:15:45 +0530

    Automation using DVC pipeline

--- src/model/model_building.py
+++ src/model/model_building.py
@@ -1,14 +1,13 @@
-import os
-import pickle
-
 import numpy as np
 import pandas as pd
+import pickle
 from sklearn.linear_model import LogisticRegression
-
+import yaml
 from src.logger import logging
 
 
 def load_data(file_path: str) -> pd.DataFrame:
+    """Load data from a CSV file."""
     try:
         df = pd.read_csv(file_path)
         logging.info('Data loaded from %s', file_path)
@@ -20,18 +19,16 @@ def load_data(file_path: str) -> pd.DataFrame:
         logging.error('Unexpected error occurred while loading the data: %s', e)
         raise
 
-
 def train_model(X_train: np.ndarray, y_train: np.ndarray) -> LogisticRegression:
     """Train the Logistic Regression model."""
     try:
-        clf = LogisticRegression(C=1, solver='liblinear', penalty='l2')
+        clf = LogisticRegression(C=1, solver='liblinear', penalty='l1')
         clf.fit(X_train, y_train)
         logging.info('Model training completed')
         return clf
     except Exception as e:
         logging.error('Error during model training: %s', e)
         raise
-
 
 def save_model(model, file_path: str) -> None:
     """Save the trained model to a file."""
@@ -43,25 +40,19 @@ def save_model(model, file_path: str) -> None:
         logging.error('Error occurred while saving the model: %s', e)
         raise
 
-
 def main():
     try:
-        train_data = load_data('./data/processed/train_bow.csv')
 
+        train_data = load_data('./data/processed/train_bow.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
 
         clf = train_model(X_train, y_train)
-
+        
         save_model(clf, 'models/model.pkl')
-
     except Exception as e:
-        logging.error(
-            'Failed to complete the model building process: %s',
-            e
-        )
+        logging.error('Failed to complete the model building process: %s', e)
         print(f"Error: {e}")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
